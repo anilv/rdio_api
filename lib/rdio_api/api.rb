7e1724108962173dba69f48ab99ffb73f6f0fa46
Anil <anilv87@gmail.com>
2011-07-22 14:20:20 -0400

    Flattening arguments to a Hash and merging with Request Body

--- lib/rdio_api/api.rb
+++ lib/rdio_api/api.rb
@@ -2,10 +2,11 @@ module RdioApi
   
   module Api
     
-    def method_missing(method_name)
+    def method_missing(method_name, *arguments)
       response = connection.post do |request|
-        request.body = { :method => method_name.to_s}
+        request.body = {:method => method_name.to_s}.merge!(Hash[*arguments.flatten])
       end
+      check_response_for_errors(response)
     end
   end
 end
