3e4384e052d8917fe9677c572a97c052c24d2b13
Anil <anilv87@gmail.com>
2011-07-27 16:09:57 -0400

    Added Fixtures and started on testing Core methods

--- spec/helper.rb
+++ spec/helper.rb
@@ -3,3 +3,15 @@ $:.unshift File.expand_path('../../lib', __FILE__)
 require 'rdio_api'
 require 'rspec'
 require 'webmock/rspec'
+
+def stub_post
+  stub_request(:post, "http://api.rdio.com/1/")
+end
+
+def fixture_path
+  File.expand_path("../fixtures", __FILE__)
+end
+
+def fixture(file)
+  File.new(fixture_path + '/' + file)
+end
---
+++ spec/rdio_api/core_spec.rb
@@ -0,0 +1,21 @@
+require 'helper'
+
+describe RdioApi::Client do
+  
+  context "when working with the Core methods" do
+    before do
+      @client = RdioApi::Client.new(:consumer_key => "consumerkey", :consumer_secret => "consumersecret")
+    end
+    
+    describe "'get'" do
+      before do
+        stub_post.with(:body => {:method => 'get', :keys => "r13700"}).
+          to_return(:body => fixture("get.json"))
+      end
+
+      it "should have the correct Artist name" do
+        @client.get(:keys => "r13700").r13700.name.should eq("John Williams")
+      end
+    end
+  end
+end
