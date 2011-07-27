fd5ae90a3aa4734454837412c690bee90a7993f1
Anil <anilv87@gmail.com>
2011-07-27 17:43:01 -0400

    Added Tests for the Catalog methods

---
+++ spec/rdio_api/catalog_spec.rb
@@ -0,0 +1,77 @@
+require 'helper'
+
+describe RdioApi::Client do
+  
+  context "when working with the Catalog methods" do
+    before do
+      @client = RdioApi::Client.new(:consumer_key => "consumerkey", :consumer_secret => "consumersecret")
+    end
+    
+    describe "'getAlbumsForArtist'" do
+      
+      context "with artist key passed" do
+        before do
+          stub_post.with(:body => {:method => 'getAlbumsForArtist', :artist => "r20227"}).
+            to_return(:body => fixture("getAlbumsForArtist.json"))
+        end
+
+        it "should return Array of Albums" do
+          albums = @client.getAlbumsForArtist(:artist => "r20227").result
+          albums.should be_an Array
+          albums.first.name.should eq("My Beautiful Dark Twisted Fantasy")
+        end
+      end
+    end
+    
+    describe "'getTracksForArtist'" do
+      
+      context "with artist key passed" do
+        before do
+          stub_post.with(:body => {:method => 'getTracksForArtist', :artist => "r311065"}).
+            to_return(:body => fixture("getTracksForArtist.json"))
+        end
+        
+        it "should return Array of Tracks" do
+          tracks = @client.getTracksForArtist(:artist => "r311065").result
+          tracks.should be_an Array
+          tracks.last.duration.should eq(121)
+        end
+      end
+    end
+    
+    describe "'search'" do
+      
+      context "with query and types passed" do
+        before do
+          stub_post.with(:body => {:method => 'search', :query => "michael giacchino", :types => "album"}).
+            to_return(:body => fixture("search.json"))
+        end
+        
+        it "should have correct track count" do
+          results = @client.search(:query => "michael giacchino", :types => "album").result
+          results.track_count.should eq(708)
+        end
+        
+        it "should include correct track for a result" do
+          results = @client.search(:query => "michael giacchino", :types => "album").result
+          results.results.first.trackKeys.should include("t9506546")
+        end
+      end
+    end
+    
+    describe "searchSuggestions" do
+      
+      context "with query passed" do
+        before do
+          stub_post.with(:body => {:method => 'searchSuggestions', :query => "yoav"}).
+            to_return(:body => fixture("searchSuggestions.json"))
+        end
+        
+        it "should the correct radioKey" do
+          suggestions = @client.searchSuggestions(:query => "yoav").result
+          suggestions.first.radioKey.should eq("rr316321")
+        end
+      end
+    end
+  end
+end
