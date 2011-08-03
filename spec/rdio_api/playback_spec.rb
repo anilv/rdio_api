4ac93b83fb470bf106c79faebb0e99479536dc8e
Anil V <anilv87@gmail.com>
2011-08-03 15:50:24 -0400

    Added tests for Playback methods

---
+++ spec/rdio_api/playback_spec.rb
@@ -0,0 +1,22 @@
+require 'helper'
+
+describe RdioApi::Client do
+  
+  context "when working with the Playback methods" do
+    before do
+      @client = RdioApi.new(:consumer_key => "consumerkey", :consumer_secret => "consumersecret")
+    end
+    
+    describe "'getPlaybackToken'" do
+      before do
+        stub_post.with(:body => {:method => 'getPlaybackToken'}).
+          to_return(:body => fixture("getPlaybackToken.json"))
+      end
+
+      it "should return a Playback Token" do
+        results = @client.getPlaybackToken
+        results.result.should eq("flkasdjFA5lkdjf90asdfli2l9cnlkasdj9")
+      end
+    end
+  end
+end
