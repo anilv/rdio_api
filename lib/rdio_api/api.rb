c9a87e3996fad5d8cd07692baf85af187714d073
Anil V <anilv87@gmail.com>
2011-08-04 00:14:23 -0400

    Added respond_to? and refactored method_missing

--- lib/rdio_api/api.rb
+++ lib/rdio_api/api.rb
@@ -6,21 +6,34 @@ module RdioApi
   module Api
     
     def method_missing(method_sym, *arguments)
-       if UNAUTHENTICATED.include?(method_sym)
-         response = unauthenticated_connection.post do |request|
-           request.body = {:method => method_sym.to_s}.merge!(Hash[*arguments.flatten])
-         end
-         response.body.result
-       elsif AUTHENTICATED.include?(method_sym)
-         if authenticated_connection
-             response = MultiJson.decode(authenticated_connection.post(api_url, {:method => method_sym.to_s}.merge!(Hash[*arguments.flatten])).body)['result']
-             response.is_a?(Hash) ? Hashie::Mash.new(response) : response
-         else
-          "Set access token at initialization or the client's access_token instance variable"
-         end
-       else
-         "Unknown Method, please refer to http://developer.rdio.com/docs/read/rest/Methods for a list"
-       end
+     if UNAUTHENTICATED.include?(method_sym)
+       unauthenticated_request(method_sym, *arguments)
+     elsif AUTHENTICATED.include?(method_sym)
+       authenticated_request(method_sym, *arguments)
+     end
+    end
+    
+    def respond_to?(method)
+      AUTHENTICATED.include?(method.to_sym) || UNAUTHENTICATED.include?(method.to_sym) ? true : false
+    end
+    
+    private
+    
+    def unauthenticated_request(method_sym, *arguments)
+      response = unauthenticated_connection.post do |request|
+        request.body = {:method => method_sym.to_s}.merge!(Hash[*arguments.flatten])
+      end
+      response.body.result
+    end
+    
+    def authenticated_request(method_sym, *arguments)
+      if authenticated_connection
+          response = MultiJson.decode(authenticated_connection.post(api_url, 
+                                      {:method => method_sym.to_s}.merge!(Hash[*arguments.flatten])).body)['result']
+          response.is_a?(Hash) ? Hashie::Mash.new(response) : response
+      else
+       "Set access token at initialization or the client's access_token instance variable"
+      end
     end
 
     UNAUTHENTICATED = [
