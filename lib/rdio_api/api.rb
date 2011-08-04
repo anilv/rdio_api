0eb40b88c9e480051dcbb440c4f8a4b188a31ab7
Anil V <anilv87@gmail.com>
2011-08-04 16:32:20 -0400

    Cleanup Part 2

--- lib/rdio_api/api.rb
+++ lib/rdio_api/api.rb
@@ -6,19 +6,19 @@ module RdioApi
   module Api
     
     def method_missing(method_sym, *arguments)
-     if UNAUTHENTICATED.include?(method_sym)
-       unauthenticated_request(method_sym, *arguments)
-     elsif AUTHENTICATED.include?(method_sym)
-       authenticated_request(method_sym, *arguments)
-     end
+      if UNAUTHENTICATED.include?(method_sym)
+        unauthenticated_request(method_sym, *arguments)
+      elsif AUTHENTICATED.include?(method_sym)
+        authenticated_request(method_sym, *arguments)
+      end
     end
-    
+
     def respond_to?(method)
       AUTHENTICATED.include?(method.to_sym) || UNAUTHENTICATED.include?(method.to_sym) ? true : false
     end
     
     private
-    
+      
     def unauthenticated_request(method_sym, *arguments)
       response = unauthenticated_connection.post do |request|
         request.body = {:method => method_sym.to_s}.merge!(Hash[*arguments.flatten])
@@ -28,11 +28,11 @@ module RdioApi
     
     def authenticated_request(method_sym, *arguments)
       if authenticated_connection
-          response = MultiJson.decode(authenticated_connection.post(api_url, 
-                                      {:method => method_sym.to_s}.merge!(Hash[*arguments.flatten])).body)['result']
-          response.is_a?(Hash) ? Hashie::Mash.new(response) : response
+        response = MultiJson.decode(authenticated_connection.post(api_url, 
+                                    {:method => method_sym.to_s}.merge!(Hash[*arguments.flatten])).body)['result']
+        response.is_a?(Hash) ? Hashie::Mash.new(response) : response
       else
-       "Set access token at initialization or the client's access_token instance variable"
+        "Set access token at initialization or the client's access_token instance variable"
       end
     end
 
